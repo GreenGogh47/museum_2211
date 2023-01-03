2c61fad909b0c53de7b343d378b052ad2f58a05f
Chris Crane <chrisjcrane@gmail.com>
2023-01-03 09:29:50 -0700

    Pass add_interests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Patron do
   end
 
   describe '#add_interest' do
-    xit 'can add interests to the array' do
+    it 'can add interests to the array' do
       patron_1.add_interest("Dead Sea Scrolls")
       patron_1.add_interest("Gems and Minerals")
       expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
