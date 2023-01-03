8a66294fb4918487797f65daa4822f5849611b13
Chris Crane <chrisjcrane@gmail.com>
2023-01-03 09:26:09 -0700

    Add patron tests

---
+++ spec/patron_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Patron do
+  let(:patron_1) { Patron.new("Bob", 20) }
+  
+  describe '#initialize' do
+    it 'exists' do
+      expect(patron_1).to be_instance_of(Patron)
+    end
+
+    xit 'has attributes' do
+      expect(patron_1.name).to eq("Bob")
+      expect(patron_1.spending_money).to eq(20)
+      expect(patron_1.interests).to eq([])
+    end
+  end
+
+  describe '#add_interest' do
+    xit 'can add interests to the array' do
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+      expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    end
+  end
+end
