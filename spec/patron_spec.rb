acc0d1b869bec834a271a31964636b977855fbc5
Chris Crane <chrisjcrane@gmail.com>
2023-01-03 09:27:31 -0700

    Pass it exists

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Patron do
       expect(patron_1).to be_instance_of(Patron)
     end
 
-    xit 'has attributes' do
+    it 'has attributes' do
       expect(patron_1.name).to eq("Bob")
       expect(patron_1.spending_money).to eq(20)
       expect(patron_1.interests).to eq([])
