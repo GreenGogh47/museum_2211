7740116b5361238c4ccb8fec4ba39205e115da48
Chris Crane <chrisjcrane@gmail.com>
2023-01-03 09:39:10 -0700

    Add tests for museum

---
+++ spec/museum_spec.rb
@@ -0,0 +1,47 @@
+require 'rspec'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+RSpec.describe Museum do
+  let(:dmns) { Museum.new("Denver Museum of Nature and Science") }
+  let(:patron_1) { Patron.new("Bob", 20) }
+  let(:patron_2) { Patron.new("Sally", 20) }
+  
+  describe '#initialize' do
+    it 'exists' do
+      expect(dmns).to be_instance_of(Museum)
+    end
+
+    it 'has attributes' do
+      expect(dmns.name).to eq("Denver Museum of Nature and Science")
+      expect(dmns.exhibits).to eq([])
+    end
+  end
+
+  describe '#add_exhitib' do
+    it 'can add exhibits to the array' do
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
+  end
+
+  describe '#recommend_exhibits' do
+    it 'recommends list of interests' do
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+      patron_2.add_interest("IMAX")
+
+      dmns.recommend_exhibits(patron_1)
+      dmns.recommend_exhibits(patron_2)
+    end
+  end
+
+end
